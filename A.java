30b2ec55d86420600fafbe146a992626a6f884f4
Álvaro Martins Espíndola <alvaromaresp@hotmail.com>
2019-09-15 13:40:11 -0300

    Classes A e B

---
+++ A.java
@@ -0,0 +1,29 @@
+public class A{
+	private int A1;
+	private float A2;
+
+	public int getA1(){
+		return A1;
+	}
+
+	public int setA1(int A1){
+		this.A1 = A1;
+	}
+
+	public int getA2(){
+		return A2;
+	}
+
+	public int setA2(int A2){
+		this.A2 = A2;
+	}
+
+	public void MA1(){
+		System.out.println("MA1");
+	}
+
+	public void MA2(){
+		System.out.println("MA2");
+	}
+
+}
