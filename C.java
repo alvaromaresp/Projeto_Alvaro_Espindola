ae1eb0decad64b9bd3f337e708827dcba919bd15
Álvaro Martins Espíndola <alvaromaresp@hotmail.com>
2019-09-15 13:46:16 -0300

    Criação da classe C

---
+++ C.java
@@ -0,0 +1,28 @@
+public class C{
+	private int C1;
+	private float C2;
+
+	public int getC1(){
+		return C1;
+	}
+
+	public int setC1(int C1){
+		this.C1 = C1;
+	}
+
+	public int getC2(){
+		return C2;
+	}
+
+	public int setC2(int C2){
+		this.C2 = C2;
+	}
+
+	public void MC1(){
+		System.out.println("MC1");
+	}
+
+	public void MC2(){
+		System.out.println("MC2");
+	}
+}
